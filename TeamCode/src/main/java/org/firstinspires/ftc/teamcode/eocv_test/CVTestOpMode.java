c702089a4f17dc1e92202fb108e39ed490bc1ded
brin <itsbrian1113@gmail.com>
2022-11-04 23:56:34 -0700

    opencv test stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/eocv_test/CVTestOpMode.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.eocv_test;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+
+@TeleOp
+public class CVTestOpMode extends OpMode {
+    private int cameraMonitorViewId;
+    private WebcamName webcamName;
+    private OpenCvCamera camera;
+    @Override
+    public void init() {
+        cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(640, 480);
+            }
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/eocv_test/Test1.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.eocv_test;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class Test1 extends OpenCvPipeline {
+
+    @Override
+    public Mat processFrame(Mat input) {
+        return null;
+    }
+}
