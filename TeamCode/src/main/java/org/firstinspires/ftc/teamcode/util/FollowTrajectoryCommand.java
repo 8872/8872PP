fca5e6e17c54184ba6f3dadcbdb9687a490b6f9c
imaspacecat <library4lion@gmail.com>
2023-02-08 23:50:37 -0800

    added rr commands

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/FollowTrajectoryCommand.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.arcrobotics.ftclib.command.CommandBase;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
+
+public class FollowTrajectoryCommand extends CommandBase {
+    private final SampleMecanumDrive drive;
+    private final Trajectory trajectory;
+
+    public FollowTrajectoryCommand(SampleMecanumDrive drive, Trajectory trajectory) {
+        this.drive = drive;
+        this.trajectory = trajectory;
+    }
+
+    @Override
+    public void initialize() {
+        drive.followTrajectoryAsync(trajectory);
+    }
+
+    @Override
+    public void execute() {
+        drive.update();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return drive.isBusy();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/FollowTrajectorySequenceCommand.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
+
+public class FollowTrajectorySequenceCommand extends CommandBase {
+    private final SampleMecanumDrive drive;
+    private final TrajectorySequence sequence;
+
+    public FollowTrajectorySequenceCommand(SampleMecanumDrive drive, TrajectorySequence sequence) {
+        this.drive = drive;
+        this.sequence = sequence;
+    }
+
+    @Override
+    public void initialize() {
+        drive.followTrajectorySequenceAsync(sequence);
+    }
+
+    @Override
+    public void execute() {
+        drive.update();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return drive.isBusy();
+    }
+}
