4f61e9b0a7ad497c6cfe1915aae0f77263f6d269
brin <itsbrian1113@gmail.com>
2023-02-16 20:50:42 -0800

    bruh opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/opmodes/BruhOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/opmodes/BruhOpmode.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.teamcode.vision.opmodes;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.RevIMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import fi.iki.elonen.NanoHTTPD;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.command.group.DownSequence;
 import org.firstinspires.ftc.teamcode.command.group.HighSequence;
@@ -13,9 +18,9 @@ import org.firstinspires.ftc.teamcode.command.group.MediumSequence;
 import org.firstinspires.ftc.teamcode.opmode.BaseOpMode;
 import org.firstinspires.ftc.teamcode.powerplayutil.Height;
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.subsystem.ArmSys;
-import org.firstinspires.ftc.teamcode.subsystem.TurretSys;
+import org.firstinspires.ftc.teamcode.subsystem.*;
 import org.firstinspires.ftc.teamcode.util.DelayedCommand;
+import org.firstinspires.ftc.teamcode.util.TriggerGamepadEx;
 import org.firstinspires.ftc.teamcode.vision.pipelines.InfoPipeline;
 import org.firstinspires.ftc.teamcode.vision.pipelines.JunctionDetection;
 import org.opencv.core.Point;
@@ -28,10 +33,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
-import java.util.function.DoubleSupplier;
 
 import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Button.*;
 import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Trigger.LEFT_TRIGGER;
@@ -39,54 +41,34 @@ import static com.arcrobotics.ftclib.gamepad.GamepadKeys.Trigger.RIGHT_TRIGGER;
 
 @Config
 @TeleOp
-public final class CameraInfoOpmode extends BaseOpMode {
-
-    private RotatedRect rect;
-
-    // NOTE: If you're using NanoHTTPD >= 3.0.0 the namespace is different,
-    //       instead of the above import use the following:
-    // import org.nanohttpd.NanoHTTPD;
-
-    public class App extends NanoHTTPD {
-        private DoubleSupplier fps;
-        public App(DoubleSupplier fps) throws IOException {
-            super(7070);
-            start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
-            System.out.println("\nRunning! Point your browsers to http://localhost:7070/ \n");
-            this.fps = fps;
-        }
-        public App() throws IOException {
-            super(7070);
-            start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
-            System.out.println("\nRunning! Point your browsers to http://localhost:7070/ \n");
-        }
-
-        @Override
-        public Response serve(IHTTPSession session) {
-            String msg = "<html><body><h1>Hello server</h1>\n";
-
-            msg += "fps: " + fps.getAsDouble() + "\n";
-
-            return newFixedLengthResponse(msg + "</body></html>\n");
-        }
-    }
-
-
-    double previousHeight = 0;
-    double previousX = 0;
-//    SampleMecanumDrive roadrunnerDrive = new SampleMecanumDrive(hardwareMap);
-
-    public static int goal = -100;
-
-    public static final double PIX_TO_DEGREE = 22.0133;
+public final class BruhOpmode extends BaseOpMode {
 
     private OpenCvCamera camera;
     private InfoPipeline pipeline;
 
     @Override
     public void initialize() {
-        super.initialize();
+        gamepadEx1 = new GamepadEx(gamepad1);
+        gamepadEx2 = new GamepadEx(gamepad2);
 
+        triggerGamepadEx1 = new TriggerGamepadEx(gamepad1, gamepadEx1);
+        triggerGamepadEx2 = new TriggerGamepadEx(gamepad2, gamepadEx2);
+
+        initHardware();
+        setUpHardwareDevices();
+
+        imu = new RevIMU(hardwareMap);
+        imu.init();
+
+        drive = new DriveSys(fL, fR, bL, bR, imu);
+        lift = new LiftSys(dr4bLeftMotor, dr4bRightMotor, limitSwitch);
+        claw = new ClawSys(clawServo);
+        arm = new ArmSys(armServo);
+        flipper = new FlipperSys(flipperServo);
+
+        rrDrive = new SampleMecanumDrive(hardwareMap);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
@@ -104,76 +86,15 @@ public final class CameraInfoOpmode extends BaseOpMode {
             }
         });
 
-        try {
-            new App(camera::getFps);
-        } catch (IOException ioe) {
-            System.err.println("Couldn't start server:\n" + ioe);
-        }
-
-
-//        gb1(LEFT_BUMPER).whileHeld(
-//                drive.slowMode(gamepadEx1::getLeftX, gamepadEx1::getRightX, gamepadEx1::getLeftY));
-//        gb1(RIGHT_BUMPER).whileHeld(
-//                drive.driveWithConeRotation(gamepadEx1::getLeftX, gamepadEx1::getRightX, gamepadEx1::getLeftY));
-
-
         gb1(DPAD_UP).whenPressed(lift.goTo(Height.HIGH));
         gb1(DPAD_LEFT).whenPressed(lift.goTo(Height.LOW));
         gb1(DPAD_RIGHT).whenPressed(lift.goTo(Height.MEDIUM));
         gb1(DPAD_DOWN).whenPressed(lift.goTo(Height.NONE));
 
-
-        gb1(A).whenPressed(new DownSequence(lift, turret, arm));
         gb1(LEFT_BUMPER).toggleWhenPressed(claw.grab().andThen(new DelayedCommand(arm.goTo(ArmSys.Pose.GRAB), 200)),
                 claw.release().andThen(new DelayedCommand(arm.goTo(ArmSys.Pose.DOWN), 200)));
 
-        // 180
-        gb1(Y).whenPressed(new HighSequence(lift, turret, arm, TurretSys.Pose.ONE_EIGHTY));
-        gb1(X).whenPressed(new MediumSequence(lift, turret, arm, TurretSys.Pose.ONE_EIGHTY));
-//        gb1(B).whenPressed(new LowSequence(lift, turret, arm, TurretSys.Pose.ONE_EIGHTY));
 
-//         forklift
-        gb1(RIGHT_BUMPER)
-                .and(gb1(Y))
-                .whenActive(lift.goTo(Height.HIGH).alongWith(arm.goTo(ArmSys.Pose.FORKLIFT)));
-        gb1(RIGHT_BUMPER)
-                .and(gb1(X))
-                .whenActive(lift.goTo(Height.MEDIUM).alongWith(arm.goTo(ArmSys.Pose.FORKLIFT)));
-
-        // front left
-        gb1(LEFT_TRIGGER).and(gb1(Y))
-                .whenActive(new HighSequence(lift, turret, arm, TurretSys.Pose.LEFT_FORWARD));
-        gb1(LEFT_TRIGGER).and(gb1(X))
-                .whenActive(new MediumSequence(lift, turret, arm, TurretSys.Pose.LEFT_FORWARD));
-        gb1(LEFT_TRIGGER).and(gb1(B))
-                .whenActive(new LowSequence(lift, turret, arm, TurretSys.Pose.LEFT_FORWARD));
-
-        // front right
-        gb1(RIGHT_TRIGGER).and(gb1(Y))
-                .whenActive(new HighSequence(lift, turret, arm, TurretSys.Pose.RIGHT_FORWARD));
-        gb1(RIGHT_TRIGGER).and(gb1(X))
-                .whenActive(new MediumSequence(lift, turret, arm, TurretSys.Pose.RIGHT_FORWARD));
-        gb1(RIGHT_TRIGGER).and(gb1(B))
-                .whenActive(new LowSequence(lift, turret, arm, TurretSys.Pose.RIGHT_FORWARD));
-
-        // back left
-        gb1(DPAD_LEFT).and(gb1(Y))
-                .whenActive(new HighSequence(lift, turret, arm, TurretSys.Pose.LEFT_BACK));
-        gb1(DPAD_LEFT).and(gb1(X))
-                .whenActive(new MediumSequence(lift, turret, arm, TurretSys.Pose.LEFT_BACK));
-        gb1(DPAD_LEFT).and(gb1(B))
-                .whenActive(new LowSequence(lift, turret, arm, TurretSys.Pose.LEFT_BACK));
-
-        // back right
-        gb1(DPAD_RIGHT).and(gb1(Y))
-                .whenActive(new HighSequence(lift, turret, arm, TurretSys.Pose.RIGHT_BACK));
-        gb1(DPAD_RIGHT).and(gb1(X))
-                .whenActive(new MediumSequence(lift, turret, arm, TurretSys.Pose.RIGHT_BACK));
-        gb1(DPAD_RIGHT).and(gb1(B))
-                .whenActive(new LowSequence(lift, turret, arm, TurretSys.Pose.RIGHT_BACK));
-
-
-        register(drive, lift, claw, turret, arm);
         drive.setDefaultCommand(drive.robotCentric(
                 gamepadEx1::getLeftX, gamepadEx1::getRightX, gamepadEx1::getLeftY));
 
@@ -182,18 +103,16 @@ public final class CameraInfoOpmode extends BaseOpMode {
     @Override
     public void run(){
         CommandScheduler.getInstance().run();
-        rect = pipeline.getRect();
+        RotatedRect rect = pipeline.getRect();
         if (rect != null) {
             telemetry.addData("center x", rect.center.x);
             telemetry.addData("center y", rect.center.y);
             telemetry.addData("height", rect.size.height);
             telemetry.addData("height width ratio", rect.size.height / rect.size.width);
-            telemetry.addData("horizontal change", rect.center.x - previousX);
-            telemetry.addData("height change", rect.size.height - previousHeight);
             telemetry.addData("fps", camera.getFps());
             telemetry.update();
             if(gamepad1.b){
-                turretServo.rotateByAngle((rect.center.x-640)/30);
+                turretServo.rotateByAngle(-(rect.center.x-640)/30);
             }
         }
 
